f77384d8034aefb7df6b946325b00ccf1bbe394d
Vedika Gupta <vedika.gupta224@gmail.com>
2024-06-19 21:22:23 +0530

    py

---
+++ A1.py/Q23.py
@@ -0,0 +1,11 @@
+c=int(input("Enter the number: "))
+
+f=(c * 1.8) + 32
+
+print(f" fahrenheit value is {f}")
+
+far=int(input("Enter the value: "))
+
+cel = (far-32)//1.8
+
+print(f" the celcius value is {cel}")
