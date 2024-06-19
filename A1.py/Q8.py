d640576c7c746c90dd7d023d1d98f8c10225a28e
Vedika Gupta <vedika.gupta224@gmail.com>
2024-06-19 14:37:37 +0530

    assignmnet

---
+++ A1.py/Q8.py
@@ -0,0 +1,5 @@
+s1="My name is "
+s2="Vedika Gupta"
+
+s3=s1+s2
+print(s3)
---
+++ L1.py
@@ -0,0 +1,11 @@
+import keyword
+
+#keyword module to see all the keywords in python version
+
+print(keyword.kwlist)
+print(len(keyword.kwlist))
+
+#taking input from user 
+
+value=input("enter value: ")
+print(value)
